b7a2988c162844c83ca23b8f6e55e20e78f91a65
Mamun <webjunior825@gmail.com>
2021-04-06 10:05:39 +0600

    something

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const cors = require('cors') ;
 const bodyParser = require('body-parser') ;
 require('dotenv').config() ;
 const app = express()
+app.use(cors()) ;
+app.use(bodyParser.json()) 
 const port = process.env.PORT || 8081
+const MongoClient = require('mongodb').MongoClient;
+const  ObjectID = require('mongodb').ObjectID ;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hsyzd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const bookCollection = client.db("bookStore").collection("books");
+    const ordersCollection = client.db("bookStore").collection("orders");
+    console.log('database connect')
+     
+  });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
