c75e745797c01ae52a1c0819da88ed23f2775f49
Mamun <webjunior825@gmail.com>
2021-04-06 10:00:36 +0600

    something

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const cors = require('cors') ;
+const bodyParser = require('body-parser') ;
+require('dotenv').config() ;
 const app = express()
 const port = process.env.PORT || 8081
 
